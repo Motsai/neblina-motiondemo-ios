c5cfb08258b4a96d7f7fa63b963a159c77c6e979
Hoan Hoang <nh.hoang@motsai.com>
2015-11-24 16:39:59 -0500

    Initial commit

---
+++ ExerciseMotionTracker/OSX/ExerciseMotionTracker/ExerciseMotionTracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ExerciseMotionTracker
+//
+//  Created by Hoan Hoang on 2015-11-23.
+//  Copyright © 2015 Hoan Hoang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
